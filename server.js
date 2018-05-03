ae5a76f3c94bb047e8c8037ded58cc38cfdbc034
ssc-mmartell <38924297+ssc-mmartell@users.noreply.github.com>
2018-05-03 09:33:50 -0500

    added process.env.PORT as place to listen

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var app = express();
 var staticPath = path.join(__dirname, '/app');
 app.use(express.static(staticPath));
 
-app.listen(5000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log('listening');
 });
